928689eb975a6bebbc251476d1c0cf714305ca9e
NhatTanVu <onchua2006@gmail.com>
2017-04-05 10:39:26 +0800

    Commit patch

---
+++ src/Sitecore.Support.110684/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.110684")]
+[assembly: AssemblyProduct("Sitecore.Support.110684")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Sitecore")]
+
